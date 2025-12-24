b1070aae8c754f1025894932ffa0aa0e08ef52fb
Erich Varnes <evarnes@atlng01.physics.arizona.edu>
2025-12-24 07:14:43 -0700

    update for new file names

--- test/ctest_turbineECalEndcap_calib.C
+++ test/ctest_turbineECalEndcap_calib.C
@@ -1,15 +1,15 @@
-#ifndef ctest_ECal_calib_C
-#define ctest_ECal_calib_C
-#include "../sampling_fractions/autoCalib_analytic_topo.C"
+#ifndef ctest_turbineECalEndcap_calib_C
+#define ctest_turbineECalEndcap_calib_C
+#include "../sampling_fractions/turbineECalEndcap_autoCalib_analytic_topo.C"
 
-void ctest_ECal_calib() {
+void ctest_turbineECalEndcap_calib() {
   TFile f("allegro_v03_ecal_v52_evts_1000_pdg_11_MomentumMinMax_40_40_GeV_ThetaMinMax_5.2_40.0_PhiMinMax_0_6.28_0_digi_reco.root");
 
   TTree * t = (TTree*) f.Get("events");
   
-  autoCalib_analytic_topo*  a= new autoCalib_analytic_topo(t); 
+  turbineECalEndcap_autoCalib_analytic_topo a(t); 
 
-  a->Calibrate();
+  a.Calibrate();
 
 }
  
